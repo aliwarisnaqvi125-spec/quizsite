b4ebd715bd5be3d624276aa11714419da0432efd
aliwarisnaqvi125-spec <aliwarisnaqvi125@gmail.com>
2026-01-26 02:29:26 -0800

    adsetera live ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: "Lumina Skin | Glow Inside Out",
@@ -16,8 +18,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="adsterra-options" strategy="beforeInteractive">
+          {`
+            var atOptions = {
+              'key' : 'a050d74ba22fc7e09ef8ff1839811f56',
+              'format' : 'iframe',
+              'height' : 60,
+              'width' : 468,
+              'params' : {}
+            };
+          `}
+        </Script>
+        <Script
+          src="https://www.highperformanceformat.com/a050d74ba22fc7e09ef8ff1839811f56/invoke.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
+        <Script id="adsterra-options-2">
+          {`
+            atOptions = {
+              'key' : '659f559181940b6047f12a9941cd6a04',
+              'format' : 'iframe',
+              'height' : 600,
+              'width' : 160,
+              'params' : {}
+            };
+          `}
+        </Script>
+        <Script
+          src="https://www.highperformanceformat.com/659f559181940b6047f12a9941cd6a04/invoke.js"
+        />
         <Header />
+        <Script src="https://pl28567852.effectivegatecpm.com/b0/95/73/b0957304a0eb75e1bae85d43c6978518.js" />
         <main style={{ minHeight: 'calc(100vh - 300px)' }}>
           {children}
         </main>
